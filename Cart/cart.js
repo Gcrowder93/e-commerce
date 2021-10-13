a7af8d8fa7fce49a6f6930de9fffa58b92b27345
Chase <Gcrowder93@gmail.com>
2021-10-13 09:50:18 -0700

    fixed all issues

--- Cart/cart.js
+++ Cart/cart.js
@@ -2,6 +2,7 @@
 import { cart } from '../data/cart-data.js';
 import { discs } from '../data/discs.js';
 import { findById } from '../utils.js';
+import { calcOrderTotal, toUSD } from '../utils.js';
 {const tbody = document.getElementById('table-body');
     for (let cartItem of cart){
         const discData = findById(cartItem.id, discs);
@@ -13,8 +14,12 @@ import { findById } from '../utils.js';
         const tdQty = document.createElement('td');
         tdQty.textContent = cartItem.qty;
         const tdTotal = document.createElement('td');
-        tdTotal.textContent = `$${cartItem.qty * discData.price}.00`;
+        tdTotal.textContent = `$${cartItem.qty * discData.price}`;
         tr.append(tdName, tdPrice, tdQty, tdTotal);
         tbody.appendChild(tr);
     }
 }
+
+const orderTotal = calcOrderTotal(cart, discs);
+const tdOrderTotal = document.getElementById('total');
+tdOrderTotal.textContent = toUSD(orderTotal);
--- utils.js
+++ utils.js
@@ -3,3 +3,17 @@ export function findById(id, discs){
         if (disc.id === id){
             return disc;
         }}}
+export function calcOrderTotal(cart, discs){
+    let orderTotal = 0;
+    for (let item of cart){
+        const disc = findById(item.id, discs);
+        orderTotal = orderTotal + disc.price * item.qty;
+    
+    }
+    return orderTotal;
+}
+
+export function toUSD(number){
+    return number.toLocaleString('en-us', { style: 'currency', currency: 'USD' }
+    ); 
+}
